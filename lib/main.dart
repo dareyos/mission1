f8810c26334540c77c945aab33bc812f88b0dc7d
Dareyos <dashabella1@gmail.com>
2024-02-20 14:35:30 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mission1/screens/account_screen.dart';
+import 'package:mission1/screens/home_screen.dart';
+import 'package:mission1/screens/login_screen.dart';
+import 'package:mission1/screens/reset_password_screen.dart';
+import 'package:mission1/screens/signup_screen.dart';
+import 'package:mission1/screens/verify_email_screen.dart';
+import 'package:mission1/services/firebase_streem.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // строка позволяет запустить firebase до запуска приложения
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+// void InitFirebase() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+
+// }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        }),
+      ),
+      routes: {
+        '/': (context) => const FirebaseStream(),
+        '/home': (context) => const HomeScreen(),
+        '/account': (context) => const AccountScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen(),
+        '/verify_email': (context) => const VerifyEmailScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
